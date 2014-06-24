5e19f230f9d3eacc8e54692cdcbe0ae3119971f7
luizventurote <luiz_17@ymail.com>
2014-06-24 01:16:45 -0300

    Hibernate adicionado.

--- src/view/MainWin.java
+++ src/view/MainWin.java
@@ -10,7 +10,7 @@ public class MainWin extends javax.swing.JFrame {
 
     
     public MainWin() {
-        initComponents();
+        initComponents();        
     }
 
     @SuppressWarnings("unchecked")
