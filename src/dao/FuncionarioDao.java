c253c31a7a3f8503e6c54d2ce16bdfed9eb108d8
luizventurote <luiz_17@ymail.com>
2014-06-25 17:47:55 -0300

    Funcionário Dao foi adicionada.

---
+++ src/dao/FuncionarioDao.java
@@ -0,0 +1,182 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import model.Funcionario;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+
+/**
+ *
+ * @author Luiz
+ */
+public class FuncionarioDao extends DefaultDao {
+    
+     public FuncionarioDao() throws Exception, SQLException, HibernateException {
+        
+        this.setTable_reference("funcionario");
+        this.object_reference = "Funcionario";
+        
+    }
+
+    /**
+     * Inserir
+     * 
+     * @param obj
+     * @throws Exception
+     * @throws SQLException
+     * @throws HibernateException 
+     */
+    public void insert(Funcionario obj) throws Exception, SQLException, HibernateException {
+
+        try {
+
+            this.session = util.HibernateUtil.getSessionFactory().openSession();
+
+            this.session.beginTransaction();
+
+            this.session.save(obj);
+
+            this.session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+    
+    /**
+     * Deletar
+     *
+     * @param obj
+     * @throws Exception
+     * @throws SQLException
+     * @throws HibernateException
+     */
+    public void delete(Funcionario obj) throws Exception, SQLException, HibernateException {
+
+        try {
+
+            this.session = util.HibernateUtil.getSessionFactory().openSession();
+
+            this.session.beginTransaction();
+
+            this.session.delete(obj);
+
+            this.session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+    
+    /**
+     * Atualizar
+     *
+     * @param obj
+     * @throws Exception
+     * @throws SQLException
+     * @throws HibernateException
+     */
+    public void update(Funcionario obj) throws Exception, SQLException, HibernateException {
+
+        try {
+
+            this.session = util.HibernateUtil.getSessionFactory().openSession();
+
+            this.session.beginTransaction();
+
+            this.session.update(obj);
+
+            this.session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+    
+    /**
+     * Return all inserted objects.
+     *
+     * @return
+     * @throws Exception
+     * @throws SQLException
+     */
+    public List<Funcionario> getAll() throws Exception, SQLException {
+
+        List list = null;
+
+        try {
+
+            this.session = util.HibernateUtil.getSessionFactory().openSession();
+
+            this.session.beginTransaction();
+
+            // HQL           
+            Query con = this.session.createQuery("FROM "+this.object_reference);
+
+            list = con.list();
+
+            this.session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            return list;
+        }
+    }
+    
+    /**
+     * Get object by ID
+     *
+     * @param id
+     * @return
+     * @throws Exception
+     * @throws SQLException
+     */
+    public Funcionario get(int id) throws Exception, SQLException {
+
+        Funcionario comp = null;
+
+        try {
+
+            this.session = util.HibernateUtil.getSessionFactory().openSession();
+
+            this.session.beginTransaction();
+
+            // HQL para recuperar o componente do banco de dados           
+            Query con = this.session.createQuery("FROM "+this.object_reference+" obj WHERE obj.id=" + id);
+
+            con.setMaxResults(1);
+
+            List<Funcionario> result = con.list();
+
+            comp = result.get(0);
+
+            this.session.getTransaction().commit();
+
+        } catch (HibernateException he) {
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+            return comp;
+        }
+    }
+    
+}
